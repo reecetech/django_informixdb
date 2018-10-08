152432aef7e76e2e77aa20cce42cd9570a244c4f
Edwin Vergara <edwin.vergara@reece.com.au>
2018-10-08 17:59:40 +1100

    Addressing deprecation warning for conversion functions in Django 2+ Closes #3

--- test/datatypes/models.py
+++ test/datatypes/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from django_informixdb.fields import TrimCharField
+
 
 class Donut(models.Model):
     name = models.CharField(max_length=100)
+    trim_name = TrimCharField(max_length=100)
     is_frosted = models.BooleanField(default=False)
     cost = models.DecimalField(decimal_places=2, max_digits=10, default=0)
